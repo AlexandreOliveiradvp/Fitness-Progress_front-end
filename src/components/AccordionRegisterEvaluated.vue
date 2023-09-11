5462a0eaf22e685357e597d988d6517e85eff9f2
Alexandre.oliveira <alexandre.oliveira@celeritech.com.br>
2023-09-11 18:21:03 -0300

    feat: starting delete request

---
+++ src/components/AccordionRegisterEvaluated.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-expansion-panels>
+    <v-expansion-panel>
+      <v-expansion-panel-title>
+        <v-row class="py-2">
+          <v-col>
+            <h3 class="title-page">Registrar Novos Avaliados</h3>
+          </v-col>
+        </v-row>
+      </v-expansion-panel-title>
+      <v-expansion-panel-text class="py-4 px-3">
+        <v-row>
+          <v-col>
+            <v-text-field
+              label="Nome do avaliado"
+              hide-details="auto"
+              variant="solo-filled"
+            ></v-text-field>
+          </v-col>
+          <v-col>
+            <v-text-field
+              label="Email do avaliado"
+              hide-details="auto"
+              variant="solo-filled"
+            ></v-text-field>
+          </v-col>
+          <v-col>
+            <v-text-field
+              label="Telefone do avaliado"
+              hide-details="auto"
+              variant="solo-filled"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-text-field
+              label="Data de nascimento"
+              hide-details="auto"
+              variant="solo-filled"
+            ></v-text-field>
+          </v-col>
+          <v-col>
+            <v-select
+              label="Sexo"
+              :items="['Masculino', 'Feminino']"
+              variant="solo-filled"
+            ></v-select>
+          </v-col>
+          <v-col></v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-btn
+              variant="tonal"
+              class="float-right bg-primary button-default"
+            >
+              Salvar
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-expansion-panel-text>
+    </v-expansion-panel>
+  </v-expansion-panels>
+</template>
+<script lang="ts" setup>
+import { defineComponent } from "vue";
+defineComponent({
+  name: "AccordionRegisterEvaluated",
+});
+</script>
--- src/views/RegisterEvalueted.vue
+++ src/views/RegisterEvalueted.vue
@@ -1,69 +1,6 @@
 <template>
   <v-container>
-    <v-expansion-panels>
-      <v-expansion-panel>
-        <v-expansion-panel-title>
-          <v-row class="py-2">
-            <v-col>
-              <h3 class="title-page">Registrar Novos Avaliados</h3>
-            </v-col>
-          </v-row>
-        </v-expansion-panel-title>
-        <v-expansion-panel-text class="py-4 px-3">
-          <v-row>
-            <v-col>
-              <v-text-field
-                label="Nome do avaliado"
-                hide-details="auto"
-                variant="solo-filled"
-              ></v-text-field>
-            </v-col>
-            <v-col>
-              <v-text-field
-                label="Email do avaliado"
-                hide-details="auto"
-                variant="solo-filled"
-              ></v-text-field>
-            </v-col>
-            <v-col>
-              <v-text-field
-                label="Telefone do avaliado"
-                hide-details="auto"
-                variant="solo-filled"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col>
-              <v-text-field
-                label="Data de nascimento"
-                hide-details="auto"
-                variant="solo-filled"
-              ></v-text-field>
-            </v-col>
-            <v-col>
-              <v-select
-                label="Sexo"
-                :items="['Masculino', 'Feminino']"
-                variant="solo-filled"
-              ></v-select>
-            </v-col>
-            <v-col></v-col>
-          </v-row>
-          <v-row>
-            <v-col>
-              <v-btn
-                variant="tonal"
-                class="float-right bg-primary button-default"
-              >
-                Salvar
-              </v-btn>
-              <v-btn variant="tonal" class="float-right me-3"> Cancelar </v-btn>
-            </v-col>
-          </v-row>
-        </v-expansion-panel-text>
-      </v-expansion-panel>
-    </v-expansion-panels>
+    <AccordionRegisterEvaluated />
     <v-row>
       <v-col class="py-8">
         <v-card>
@@ -75,6 +12,7 @@
                 <th class="text-left">Telefone</th>
                 <th class="text-left">Data de Nascimento</th>
                 <th class="text-left">Sexo</th>
+                <th class="text-left">Ações</th>
               </tr>
             </thead>
             <tbody>
@@ -84,6 +22,14 @@
                 <td>{{ evaluated.phone }}</td>
                 <td>{{ evaluated.dateofbirth }}</td>
                 <td>{{ evaluated.sex }}</td>
+                <td>
+                  <v-btn
+                    variant="tonal"
+                    class="delete-button"
+                    @click="deleteEvaluated(evaluated.id)"
+                    ><Icon icon="tabler:trash-filled" class="icon-trash"
+                  /></v-btn>
+                </td>
               </tr>
             </tbody>
           </v-table>
@@ -95,11 +41,13 @@
 <script lang="ts" setup>
 import { defineComponent, onMounted, ref } from "vue";
 import api from "@/api/api";
+import AccordionRegisterEvaluated from "@/components/AccordionRegisterEvaluated.vue";
+import { Icon } from "@iconify/vue";
 defineComponent({
   name: "RegisterEvaluated",
 });
 const evaluateds = ref();
-onMounted(() => {
+const getEvaluateds = () => {
   api
     .get("/evaluated")
     .then((response) => {
@@ -108,5 +56,12 @@ onMounted(() => {
     .catch((err) => {
       console.log(err);
     });
+};
+const deleteEvaluated = (id: number) => {
+  api.delete(`/evaluated/${id}`).then(() => {});
+  console.log(id);
+};
+onMounted(() => {
+  getEvaluateds();
 });
 </script>
